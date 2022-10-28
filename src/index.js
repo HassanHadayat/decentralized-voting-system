49383ef6a2954f77cdd18e1107e8a36f35a43085
Hassan Hadayat <hassanhadayat3@gmail.com>
2022-10-28 23:27:54 +0500

    project cleaned

--- src/index.js
+++ src/index.js
@@ -3,20 +3,11 @@ import ReactDOM from 'react-dom/client';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
-import HomePage from './components/HomePage/HomePage';
-import LoginPage from './components/LoginTask/LoginPage';
-import reportWebVitals from './reportWebVitals';
+import HomePage from './components/HomePage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <HomePage />
-    {/* <App /> */}
-    {/* <LoginPage/> */}
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
