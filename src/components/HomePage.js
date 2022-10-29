6f8ad3057419d531be617d1d0e2e8f1f92ae3b3b
Hassan Hadayat <hassanhadayat3@gmail.com>
2022-10-30 00:29:47 +0500

    landing page css name changed

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,51 +1,38 @@
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import './HomePage.css';
-import logo from '../assets/images/logo.png'
-import middleImg from '../assets/images/middle-img.png'
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import "./HomePage.css";
+import logo from "../assets/images/logo.png";
+import middleImg from "../assets/images/middle-img.png";
 
 function HomePage() {
   return (
-    <div className='wrapper'>
-    <Container className='header-new'>
-      <Row className='mx-3'>
-        <Row className='mx-3'><h1>Decentralized Voting System</h1></Row>
-        {/* <Col></Col> */}
-      </Row>
-      <Row className='mx-3'>
-      <img className='logo' src={logo} alt="logo" />
-      </Row>
-      <Row>
-        <Col className='hero'>
-          <Row>
-            <img className='middleImg' src={middleImg} alt="middle Image" />
+    <div className="wrapper">
+      <Container className="header">
+        <Row className="mx-3">
+          <Row className="mx-3">
+            <h1>Decentralized Voting System</h1>
           </Row>
-          <Row className='hero-btns'>
-            <Col className='signIn-btn'><button >Sign In</button></Col>
-            <Col  className='reg-btn'><button>Register</button></Col>
-          </Row>
-        </Col>
-      </Row>
-    </Container>
-  </div>
+        </Row>
 
-    
-    // <div className='wrapper'>
-    //   <div className='header'>
-    //     <ul>
-    //     <li><h1>Decentralized Voting System</h1></li>
-    //     <li><img className='logo' src={logo} alt="logo" /></li>
-    //     </ul>
-    //   </div>
-    //   <div className='hero'>
-    //     <div>
-    //       <img className='middleImg' src={middleImg} alt="middle Image" />
-    //       <button>Register</button>
-    //       <button>Sign In</button>
-    //     </div>
-    //   </div>
-    // </div>
+        <Row className="mx-3">
+          <img className="logo" src={logo} alt="logo" />
+        </Row>
+
+        <Row>
+          <Col className="home-page">
+            <Row>
+              <img className="middle-img" src={middleImg} alt="election home page image" />
+            </Row>
+            <Row>
+              <Col className="signin-btn"> <button>Sign In</button> </Col>
+              <Col className="reg-btn"> <button>Register</button> </Col>
+            </Row>
+          </Col>
+        </Row>
+        
+      </Container>
+    </div>
   );
 }
 
