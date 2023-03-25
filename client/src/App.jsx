78fa12da3cd2d9770b7d56af6fe5aa8082f7e70b
Hassan Hadayat <hassanhadayat3@gmail.com>
2023-03-25 12:19:39 +0500

    setup for FYP-2 new design

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,6 @@
 import { EthProvider } from "./contexts/EthContext";
 import { UserProvider } from "./contexts/UserContext";
 import { Routes, Route } from "react-router-dom";
-import { LandingPage, HomePage, ElectionsPage, ResultsPage, CreatePoll, PollPage, ResultPage, UserProfile} from "./components/pages";
 import "./App.css";
 
 function App() {  
@@ -10,14 +9,7 @@ function App() {
       <UserProvider>
         <Routes>
           <Route>
-            <Route path="/" element={<LandingPage />} />
-            <Route path="/Home" element={<HomePage />} />
-            <Route path="/Elections" element={<ElectionsPage />} />
-            <Route path="/Results" element={<ResultsPage />} />
-            <Route path="/CreatePoll" element={<CreatePoll />} />
-            <Route path="/Elections/:id" element={<PollPage />} />
-            <Route path="/Results/:id" element={<ResultPage />} />
-            <Route path="/UserProfile" element={<UserProfile />} />
+            {/* <Route path="/" element={<LandingPage />} /> */}
           </Route>
         </Routes>
       </UserProvider>
